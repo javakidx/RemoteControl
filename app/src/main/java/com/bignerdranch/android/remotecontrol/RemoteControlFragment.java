5174a48a700a910d9b71178eb7e888976904a71c
javakidx <javakidx@gmail.com>
2015-08-09 12:49:39 +0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/remotecontrol/RemoteControlFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.remotecontrol;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by bioyang on 15/8/9.
+ */
+public class RemoteControlFragment extends Fragment
+{
+}
